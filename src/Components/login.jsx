d562b2c2e006249c5454380e6f7a8eb709dc9349
Cyril-Cristo-Johns <cyrilcristojohns@gmail.com>
2026-09-13 13:14:41 +0530

    Login and SignUp page

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 
 const Login= ()=>{
 
@@ -43,21 +43,21 @@ const Login= ()=>{
 
     return (
         <>
-        <form  onSubmit={handleSubmit} className="border rounded-2xl  p-2 flex flex-col justify-center items-center gap-1 max-w-lg m-auto shadow-xl">
+        <form  onSubmit={handleSubmit} className="border rounded-2xl  p-6 flex flex-col justify-center items-center gap-4 max-w-lg m-auto shadow-xl">
 
         <h1 className="text-center text-3xl p-2 ">Log In </h1>
             <div className="flex flex-col gap-1 text-left">
 
             <label htmlFor="name" className="text-sm text-gray-600">Name:</label>
             <input type="text" id="name" onChange={handleChange} value={user.name}
-            className="border border-black rounded-md p-1 "/>
+            className="border border-gray-200 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 "/>
             </div>
 
           <div className="flex flex-col gap-1 text-left">
 
-            <label htmlFor="emailInputContainer" className="text-sm text-gray-600">Email:</label>
+            <label htmlFor="email" className="text-sm text-gray-600">Email:</label>
             <input type="email" id="email" onChange={handleChange} value={user.email}
-            className="border border-black rounded-md p-1 "/>
+            className="border border-gray-200 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-600"/>
             </div>
 
 
@@ -65,15 +65,15 @@ const Login= ()=>{
 
             <label htmlFor="password" className="text-sm text-gray-600">Password:</label>
             <input type="password" id="password"  onChange={handleChange} value={user.password}
-            className="border border-black rounded-md p-1 "/>
+            className="border border-gray-200 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-600"/>
             </div>
 
 
-            <button type="submit" className="bg-black rounded-md m-2 text-white p-2 "> Login</button>
+            <button type="submit" className="bg-blue-600 rounded-md m-2 hover:bg-blue-700 text-white p-3 "> Login</button>
 
         {(status.success)?
-        <p className="text-green-500">Logged In successfully</p>
-        :<p className="text-red-500">Error: {status.error}</p>
+        <p className="text-green-500">✓Logged In successfully</p>
+        :(status.error)&&<p className="text-red-500">⚠Error: {status.error}</p>
         }
         </form>
         </>
