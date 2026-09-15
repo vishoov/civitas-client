e75256269f23cf89dc8cf39a6a3e2b16bee0d07d
Vishoo <vverma971@gmail.com>
2026-09-15 12:20:05 +0530

    fixxessss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import Login from "./Components/login"
 import SignUp from "./Components/SignUp"
 import { Route, Routes } from "react-router-dom"
-import Homepage from "./Components/homepage"
-import Protected from "./Components/Protected"
 import { AuthProtected } from "./Auth/AuthContext"
-import { Dashboard } from "./Components/Dashboard"
 import AdminDashboard from "./component/adminDashboard"
 import Navbar from "./component/navbar"
 import ReportPage from "./component/reportPage"
@@ -15,22 +12,20 @@ import Home from "./pages/Home"
 const App = () => {
   return (
     <div>
-      <h1>CIVITAS Home page!!</h1>
 
+
+      <Navbar/>
 
       <AuthProtected>
       <Routes>
-        <Route element={<Protected/>} >
-          <Route path="/dashboard" element={<Dashboard/>} />
-        </Route>
+
         <Route path="/signup" element={<SignUp />}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/homepage" element={<Homepage/>} />
-        <Route path="*" element={<Homepage />} />
+
+
       </Routes>
 
       </AuthProtected>
-      <Navbar/>
 
 
       <Routes>
