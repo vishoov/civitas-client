6531b961e9042415e84433e1334e6fbc39129d6c
Cyril-Cristo-Johns <cyrilcristojohns@gmail.com>
2026-09-15 10:54:58 +0530

    App.jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import Login from "./Components/login"
+import SignUp from "./Components/SignUp"
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./Components/homepage"
+import Protected from "./Components/Protected"
+import { AuthProtected } from "./Auth/AuthContext"
+import { Dashboard } from "./Components/Dashboard"
 
 
 const App = () => {
   return (
     <div>
       <h1>CIVITAS Home page!!</h1>
-      <Login />
+
+
+      <AuthProtected>
+      <Routes>
+        <Route element={<Protected/>} >
+          <Route path="/dashboard" element={<Dashboard/>} />
+        </Route>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/homepage" element={<Homepage/>} />
+        <Route path="*" element={<Homepage />} />
+      </Routes>
+
+      </AuthProtected>
     </div>
   )
 }
